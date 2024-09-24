ce7799d30dd3339e7405279fe34524b4ac55651b
leroykim <leroy.kim@umbc.edu>
2024-09-24 11:45:51 -0400

    Update GCP configuration

--- literature_analysis/gcp_configuration/equivalent_code_cmd.bash
+++ literature_analysis/gcp_configuration/equivalent_code_cmd.bash
@@ -1,14 +1,14 @@
-gcloud compute instances create team-nassar-literature \
+gcloud compute instances create team-nassar-literaeture \
     --project=ncbi-2024-amr-codeathon \
-    --zone=us-east4-b \
-    --machine-type=a3-highgpu-2g \
-    --network-interface=network-tier=PREMIUM,nic-type=GVNIC,stack-type=IPV4_ONLY,subnet=default \
+    --zone=us-central1-f \
+    --machine-type=a2-highgpu-4g \
+    --network-interface=network-tier=PREMIUM,stack-type=IPV4_ONLY,subnet=default \
     --maintenance-policy=TERMINATE \
     --provisioning-model=STANDARD \
     --service-account=436758014946-compute@developer.gserviceaccount.com \
     --scopes=https://www.googleapis.com/auth/devstorage.read_only,https://www.googleapis.com/auth/logging.write,https://www.googleapis.com/auth/monitoring.write,https://www.googleapis.com/auth/service.management.readonly,https://www.googleapis.com/auth/servicecontrol,https://www.googleapis.com/auth/trace.append \
-    --accelerator=count=2,type=nvidia-h100-mega-80gb \
-    --create-disk=auto-delete=yes,boot=yes,device-name=instance-20240924-135919,disk-resource-policy=projects/ncbi-2024-amr-codeathon/regions/us-central1/resourcePolicies/default-schedule-1,image=projects/ml-images/global/images/c0-deeplearning-common-gpu-v20240730-debian-11-py310,mode=rw,size=50,type=pd-balanced \
+    --accelerator=count=4,type=nvidia-tesla-a100 \
+    --create-disk=auto-delete=yes,boot=yes,device-name=instance-20240924-142427,disk-resource-policy=projects/ncbi-2024-amr-codeathon/regions/us-central1/resourcePolicies/default-schedule-1,image=projects/ml-images/global/images/c0-deeplearning-common-gpu-v20240730-debian-11-py310,mode=rw,size=500,type=pd-balanced \
     --no-shielded-secure-boot \
     --shielded-vtpm \
     --shielded-integrity-monitoring \
